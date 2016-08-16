7a02283d04041dc1a6d8d1e03683ed7a2678fa77
Visser <jannisvisser@redcross.nl>
2016-08-16 14:54:55 +0200

    latest version: administrative hierarchy, color-coding, icons, etc.

--- app/routes/dashboards.server.routes.js
+++ app/routes/dashboards.server.routes.js
@@ -10,13 +10,14 @@ module.exports = function(app) {
 
 	app.route('/dashboards/:dashboardId')
 	    .get(dashboards.hasAuthorization, dashboards.read)
+	    //.get(dashboards.hasAuthorization, dashboards.read, dashboards.postgisData)
 		.put(users.requiresLogin, dashboards.hasAuthorization, dashboards.update)
 	    .delete(users.requiresLogin, dashboards.hasAuthorization, dashboards.delete);
 		
 	app.route('/dashboards')
 		.get(dashboards.list)
 		.post(users.requiresLogin, dashboards.hasCreateAuthorization, dashboards.create);
-
+		
 	// Finish by binding the article middleware
 	app.param('dashboardId', dashboards.dashboardByID);
 };

e1b7be571c40a1f9b0b223f7a7a33a26cc90771d
Visser <jannisvisser@redcross.nl>
2016-12-21 15:54:01 +0100

    organized SQL scripts and added placeholders

--- public/modules/dashboards/controllers/districtdashboard.client.controller.js
+++ public/modules/dashboards/controllers/districtdashboard.client.controller.js
@@ -7,7 +7,7 @@ angular.module('dashboards')
 		//Define variables
 		$scope.authentication = Authentication;
 		$scope.geom = null;
-		$scope.country_code = 'NP';
+		$scope.country_code = 'PH';
 		if ($rootScope.country_code) { $scope.country_code = $rootScope.country_code;}
 		$scope.admlevel = 2;
 		$scope.metric = '';

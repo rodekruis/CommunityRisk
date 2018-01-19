0881312079327708d4a06c8cc1e3b43044a91f9a
Visser <jannisvisser@redcross.nl>
2018-01-19 15:07:56 +0100

    added Peru

--- postgres_scripts/pg_import_csv.py
+++ postgres_scripts/pg_import_csv.py
@@ -1,14 +1,14 @@
 from sqlalchemy import create_engine
 import pandas as pd
 
-#pathname = 'C:/github/Priority_index_pipeline/' #DON'T FORGET CLOSING SLASH /
-pathname = 'C:/Users/JannisV/Dropbox (510)/510 - files/Projects/Community Risk Assessment/Data/CRA - Operational Data/4. Output Layer/Ecuador/'
+#pathname = 'C:/github/' #DON'T FORGET CLOSING SLASH /
+pathname = 'C:/Users/JannisV/Dropbox (510)/510 - files/Projects/Community Risk Assessment/Data/CRA - Operational Data/4. Output Layer/Peru/'
 #pathname = 'C:/github/profiles/data/public/' #DON'T FORGET CLOSING SLASH /
-filename = 'EC_population_poverty_admin3.csv'
+filename = 'Population_inei_government.csv'
 path = pathname+filename
-schema_name = 'ecu_source' #'geo_source' / 'ph_source' / 'mw_source' / 'np_source'
+schema_name = 'per_source' #'geo_source' / 'ph_source' / 'mw_source' / 'np_source'
 table_name = 'Indicators_3_population'
-delim = ',' #',' / ';'
+delim = ';' #',' / ';'
 
 engine = create_engine('postgresql://profiles:R3dCross+83@localhost/profiles')
 df = pd.read_csv(path,delimiter=delim, encoding="windows-1251")

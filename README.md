a93da2a4f4c16122a07c2c2f663d1cae6df9938d
Visser <jannisvisser@redcross.nl>
2018-02-07 09:40:12 +0100

    changed license to GPL

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# License
+Code is created by 510 and is available under the [GPL license](https://github.com/rodekruis/communityprofiles/blob/development/LICENSE.md)
+
 # Before You Begin 
 Before you begin we recommend you read about the basic building blocks that assemble this application 
 * MongoDB - Go through [MongoDB Official Website](http://mongodb.org/) and proceed to their [Official Manual](http://docs.mongodb.org/manual/), which should help you understand NoSQL and MongoDB better.
--- postgres_scripts/meta_pg_import_csv.py
+++ postgres_scripts/meta_pg_import_csv.py
@@ -1,14 +1,12 @@
 from sqlalchemy import create_engine
 import pandas as pd
 
-#pathname = 'C:/github/' #DON'T FORGET CLOSING SLASH /
-#pathname = 'C:/Users/JannisV/Dropbox (510)/510 - files/Projects/Community Risk Assessment/Data/CRA - Operational Data/4. Output Layer/Peru/'
 pathname = 'C:/github/profiles/data/public/' #DON'T FORGET CLOSING SLASH /
 filename = 'metadata_prototype.csv'
 path = pathname+filename
-schema_name = 'metadata' #'metadata' / 'geo_source' / 'ph_source' / 'mw_source' / 'np_source'
-table_name = 'DPI_metadata' #'DPI_metadata'
-delim = ';' #',' / ';'
+schema_name = 'metadata' 
+table_name = 'DPI_metadata'
+delim = ';' 
 
 engine = create_engine('postgresql://profiles:R3dCross+83@localhost/profiles')
 df = pd.read_csv(path,delimiter=delim, encoding="windows-1251")

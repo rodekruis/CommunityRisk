af069dab03c4903199a53d6b7d63724157d1a8ff
RubenGeo <rubenavalk@gmail.com>
2019-02-19 10:00:27 +0100

    Some cleanup after login functionality

--- app/controllers/users/users.authorization.server.controller.js
+++ app/controllers/users/users.authorization.server.controller.js
@@ -4,54 +4,53 @@
  * Module dependencies.
  */
 var _ = require('lodash'),
-    mongoose = require('mongoose'),
     User = require("../../models/user.server.model")
 
 /**
  * User middleware
  */
 exports.userByID = function(req, res, next, id) {
-	User.findById(id).exec(function(err, user) {
-		if (err) return next(err);
-		if (!user) return next(new Error('Failed to load User ' + id));
-		req.profile = user;
-		next();
-	});
+    User.findById(id).exec(function(err, user) {
+        if (err) return next(err);
+        if (!user) return next(new Error('Failed to load User ' + id));
+        req.profile = user;
+        next();
+    });
 };
 
 /**
  * Require login routing middleware
  */
 exports.requiresLogin = function(req, res, next) {
-	if (!req.isAuthenticated()) {
-		return res.status(401).send({
-			message: 'User is not logged in'
-		});
-	}
+    if (!req.isAuthenticated()) {
+        return res.status(401).send({
+            message: 'User is not logged in'
+        });
+    }
 
-	next();
+    next();
 };
 
 exports.requiresTest = function(req, res, next) {
-console.log('this matches');
-	next();
+    console.log('this matches');
+    next();
 };
 
 /**
  * User authorizations routing middleware
  */
 exports.hasAuthorization = function(roles) {
-	var _this = this;
+    var _this = this;
 
-	return function(req, res, next) {
-		_this.requiresLogin(req, res, function() {
-			if (_.intersection(req.user.roles, roles).length) {
-				return next();
-			} else {
-				return res.status(403).send({
-					message: 'User is not authorized'
-				});
-			}
-		});
-	};
+    return function(req, res, next) {
+        _this.requiresLogin(req, res, function() {
+            if (_.intersection(req.user.roles, roles).length) {
+                return next();
+            } else {
+                return res.status(403).send({
+                    message: 'User is not authorized'
+                });
+            }
+        });
+    };
 };
--- app/controllers/users/users.profile.server.controller.js
+++ app/controllers/users/users.profile.server.controller.js
@@ -13,44 +13,43 @@ var _ = require('lodash'),
  * Update user details
  */
 exports.update = function(req, res) {
-	// Init Variables
-	var user = req.user;
-	var message = null;
+    // Init Variables
+    var user = req.user;
 
-	// For security measurement we remove the roles from the req.body object
-	delete req.body.roles;
+    // For security measurement we remove the roles from the req.body object
+    delete req.body.roles;
 
-	if (user) {
-		// Merge existing user
-		user = _.extend(user, req.body);
-		user.updated = Date.now();
-		user.displayName = user.firstName + ' ' + user.lastName;
+    if (user) {
+        // Merge existing user
+        user = _.extend(user, req.body);
+        user.updated = Date.now();
+        user.displayName = user.firstName + ' ' + user.lastName;
 
-		user.save(function(err) {
-			if (err) {
-				return res.status(400).send({
-					message: errorHandler.getErrorMessage(err)
-				});
-			} else {
-				req.login(user, function(err) {
-					if (err) {
-						res.status(400).send(err);
-					} else {
-						res.json(user);
-					}
-				});
-			}
-		});
-	} else {
-		res.status(400).send({
-			message: 'User is not signed in'
-		});
-	}
+        user.save(function(err) {
+            if (err) {
+                return res.status(400).send({
+                    message: errorHandler.getErrorMessage(err)
+                });
+            } else {
+                req.login(user, function(err) {
+                    if (err) {
+                        res.status(400).send(err);
+                    } else {
+                        res.json(user);
+                    }
+                });
+            }
+        });
+    } else {
+        res.status(400).send({
+            message: 'User is not signed in'
+        });
+    }
 };
 
 /**
  * Send User
  */
 exports.me = function(req, res) {
-	res.json(req.user || null);
+    res.json(req.user || null);
 };

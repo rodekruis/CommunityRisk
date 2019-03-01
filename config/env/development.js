4ef72ba0f143907b384e324429948de83d3abf80
Elwin Schmitz <eschmitz@redcross.nl>
2019-03-01 10:59:18 +0100

    Remove all code related to oAuth/Social-login

--- config/env/development.js
+++ config/env/development.js
@@ -24,34 +24,6 @@ module.exports = {
     title: "Rode Kruis Dashboards - development",
     favicon: "/modules/core/img/510-logo_inverted_32x32.png",
   },
-  facebook: {
-    clientID: process.env.FACEBOOK_ID || "APP_ID",
-    clientSecret: process.env.FACEBOOK_SECRET || "APP_SECRET",
-    callbackURL: "http://localhost:3000/auth/facebook/callback",
-  },
-  twitter: {
-    clientID: process.env.TWITTER_KEY || "CONSUMER_KEY",
-    clientSecret: process.env.TWITTER_SECRET || "CONSUMER_SECRET",
-    callbackURL: "http://localhost:3000/auth/twitter/callback",
-  },
-  google: {
-    clientID: process.env.GOOGLE_ID || "APP_ID",
-    clientSecret: process.env.GOOGLE_SECRET || "APP_SECRET",
-    callbackURL: "http://localhost:3000/auth/google/callback",
-  },
-  linkedin: {
-    clientID: process.env.LINKEDIN_ID || "APP_ID",
-    clientSecret: process.env.LINKEDIN_SECRET || "APP_SECRET",
-    callbackURL: "http://localhost:3000/auth/linkedin/callback",
-  },
-  azure: {
-    clientID: secrets.azure.clientID,
-    clientSecret: secrets.azure.clientSecret,
-    tenantId: secrets.azure.tenantId,
-    resource: "https://graph.windows.net",
-    redirectURL: "http://localhost:3000/auth/azure/callback",
-    redirectURLSSL: "http://localhost:3000/auth/azure/callback",
-  },
   onedrive: {
     oneDriveBusinessBaseUrl: "https://rodekruis-my.sharepoint.com/_api/v2.0",
   },
--- config/env/staging.js
+++ config/env/staging.js
@@ -84,34 +84,6 @@ module.exports = {
     css: "public/dist/application.min.css",
     js: "public/dist/application.min.js",
   },
-  facebook: {
-    clientID: process.env.FACEBOOK_ID || "APP_ID",
-    clientSecret: process.env.FACEBOOK_SECRET || "APP_SECRET",
-    callbackURL: "http://localhost:3000/auth/facebook/callback",
-  },
-  twitter: {
-    clientID: process.env.TWITTER_KEY || "CONSUMER_KEY",
-    clientSecret: process.env.TWITTER_SECRET || "CONSUMER_SECRET",
-    callbackURL: "http://localhost:3000/auth/twitter/callback",
-  },
-  google: {
-    clientID: process.env.GOOGLE_ID || "APP_ID",
-    clientSecret: process.env.GOOGLE_SECRET || "APP_SECRET",
-    callbackURL: "http://localhost:3000/auth/google/callback",
-  },
-  linkedin: {
-    clientID: process.env.LINKEDIN_ID || "APP_ID",
-    clientSecret: process.env.LINKEDIN_SECRET || "APP_SECRET",
-    callbackURL: "http://localhost:3000/auth/linkedin/callback",
-  },
-  azure: {
-    clientID: secrets.azure.clientID,
-    clientSecret: secrets.azure.clientSecret,
-    tenantId: secrets.azure.tenantId,
-    resource: "https://graph.windows.net",
-    redirectURL: "http://localhost:3000/auth/azure/callback",
-    redirectURLSSL: "http://localhost:3000/auth/azure/callback",
-  },
   onedrive: {
     oneDriveBusinessBaseUrl: "https://rodekruis-my.sharepoint.com/_api/v2.0",
   },

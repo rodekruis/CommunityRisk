d3d056d822fa2da4f1043762280699daed01422e
Elwin Schmitz <eschmitz@redcross.nl>
2019-03-19 15:47:51 +0100

    Add RC Locations as POI to the map

--- public/modules/dashboards/controllers/fbf.client.controller.js
+++ public/modules/dashboards/controllers/fbf.client.controller.js
@@ -94,6 +94,7 @@ angular.module("dashboards").controller("FbfController", [
       color: "#0080ff",
     };
     $scope.stations = [];
+    $scope.rcLocations = [];
 
     ///////////////////////
     // INITIAL FUNCTIONS //
@@ -182,6 +183,7 @@ angular.module("dashboards").controller("FbfController", [
       });
 
       prepareStationsData();
+      prepareRcLocationsData();
     };
 
     ///////////////
@@ -2277,6 +2279,15 @@ angular.module("dashboards").controller("FbfController", [
       return $scope.stations;
     }
 
+    function prepareRcLocationsData() {
+      $scope.rcLocations = AuthData.getPoi(
+        $scope.country_code,
+        "dashboard_redcross_branches"
+      );
+
+      return $scope.rcLocations;
+    }
+
     function createMarker(item, itemTitle, itemClass) {
       return L.marker(
         [item.geometry.coordinates[1], item.geometry.coordinates[0]],
@@ -2323,6 +2334,48 @@ angular.module("dashboards").controller("FbfController", [
 
     $scope.hide_glofas_stations = function() {
       map.removeLayer($scope.stationsLayer);
+    };
+
+    $scope.prepare_rc_locations = function() {
+      $scope.rcLocationsLayer = L.layerGroup();
+      $scope.rcLocations.forEach(function(item) {
+        if (!item.properties) return;
+
+        var location = item.properties;
+        var locationTitle = location.branch_name;
+        var locationInfoPopup =
+          "<strong class='h4'>" +
+          locationTitle +
+          "</strong><br>" +
+          "<strong>" +
+          "President: " +
+          "</strong>" +
+          location.president +
+          "<br>" +
+          "<strong>" +
+          "Volunteers: " +
+          "</strong>" +
+          location.nr_volunteers +
+          "<br>" +
+          "<strong>" +
+          "Address: " +
+          "</strong>" +
+          location.address +
+          "";
+
+        var locationMarker = createMarker(item, locationTitle, "rc");
+
+        locationMarker.addTo($scope.rcLocationsLayer);
+        locationMarker.bindPopup(locationInfoPopup);
+      });
+    };
+
+    $scope.show_rc_locations = function() {
+      map.addLayer($scope.rcLocationsLayer);
+    };
+
+    $scope.hide_rc_locations = function() {
+      map.removeLayer($scope.rcLocationsLayer);
     };
   },
 ]);

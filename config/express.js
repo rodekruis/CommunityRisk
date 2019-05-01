c260a61b198415f068141c5ff895d31eaa42b73c
Elwin Schmitz <eschmitz@redcross.nl>
2019-05-01 11:31:35 +0200

    Remove extraneous space to generate correct urls

--- config/express.js
+++ config/express.js
@@ -40,7 +40,7 @@ module.exports = function() {
 
   // Passing the request url to environment locals
   app.use(function(req, res, next) {
-    res.locals.url = req.protocol + ":// " + req.headers.host + req.url;
+    res.locals.url = req.protocol + "://" + req.headers.host + req.url;
     next();
   });
 

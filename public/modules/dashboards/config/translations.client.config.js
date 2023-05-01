a23c65ee7b1fbaf9e4fda8987849ca154be32ffe
jannisvisser <jannisvisser@redcross.nl>
2023-05-01 15:31:59 +0200

    fix: shorter warning text for mobile

--- public/modules/dashboards/config/translations.client.config.js
+++ public/modules/dashboards/config/translations.client.config.js
@@ -77,7 +77,9 @@ var static_en = {
   signup_mailing: "Sign up to mailing list",
   join_whatsapp: "Join WhatsApp group",
   warning_banner:
-    "Please be warned that this product has not been updated recently. Some datasets may still be valuable, but others are not. Browse through the indicators with the info-icon, to find out more about the source and the date. Contact ibf-support@510.global for any questions.",
+    "Warning: Some information may be outdated. Click the info icon on each indicator for source and date. Contact ibf-support@510.global for questions.",
+  warning_banner_mobile:
+    "Warning: Some information may be outdated. Contact ibf-support@510.global for questions.",
 };
 
 var static_es = {
@@ -144,7 +146,9 @@ var static_es = {
   export_json: "Exportar (JSON)",
   export_pdf: "Exportar (PDF)",
   warning_banner:
-    "Tenga en cuenta que este producto no se ha actualizado recientemente. Algunos conjuntos de datos pueden seguir siendo valiosos, pero otros no. Navegue a través de los indicadores con el ícono de información para obtener más información sobre la fuente y la fecha. Póngase en contacto con ibf-support@510.global si tiene alguna pregunta.",
+    "Advertencia: alguna información puede estar desactualizada. Haga clic en el ícono de información en cada indicador para la fuente y la fecha. Póngase en contacto con ibf-support@510.global si tiene preguntas.",
+  warning_banner_mobile:
+    "Advertencia: alguna información puede estar desactualizada. Póngase en contacto con ibf-support@510.global si tiene preguntas.",
 };
 
 var static_fr = {
@@ -212,7 +216,9 @@ var static_fr = {
   draft: "Version temporaire",
   export_json: "Exporter (JSON)",
   warning_banner:
-    "Veuillez noter que ce produit n'a pas été mis à jour récemment. Certains ensembles de données peuvent encore être utiles, mais d'autres ne le sont pas. Parcourez les indicateurs avec l'icône d'information, pour en savoir plus sur la source et la date. Contactez ibf-support@510.global pour toute question.",
+    "Attention : Certaines informations peuvent être obsolètes. Cliquez sur l'icône d'information sur chaque indicateur pour la source et la date. Contactez ibf-support@510.global pour toute question.",
+  warning_banner_mobile:
+    "Attention : Certaines informations peuvent être obsolètes. Contactez ibf-support@510.global pour toute question.",
 };
 
 angular.module("dashboards").config(function($translateProvider, DataProvider) {
